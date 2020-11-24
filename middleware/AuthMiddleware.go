82d876b7956b2f63d3cdf634f2aa0e17d6e8b463
feiyuWeb <709463253@qq.com>
2020-11-24 18:29:39 +0800

    注册api

--- middleware/AuthMiddleware.go
+++ middleware/AuthMiddleware.go
@@ -16,7 +16,7 @@ func AuthMiddleware() gin.HandlerFunc{
 		fmt.Print(tokenString,"token")
 		// 验证token格式，token要Bearer 开头
 		if tokenString == "" || !strings.HasPrefix(tokenString,"Bearer "){
-			ctx.JSON(http.StatusUnauthorized,gin.H{"code":401,"msg":"token未携带或格式不对"})
+			ctx.JSON(http.StatusUnauthorized,gin.H{"code":http.StatusUnauthorized,"msg":"token未携带或格式不对"})
 			ctx.Abort() // 中断请求
 			return
 		}
@@ -26,7 +26,7 @@ func AuthMiddleware() gin.HandlerFunc{
 		token,claims,err := common.ParseToken(tokenString)
 		if err != nil || !token.Valid{
 			fmt.Println(err,"err",token.Valid)
-			ctx.JSON(http.StatusUnauthorized,gin.H{"code":401,"msg":"token已过期，请重新登录"})
+			ctx.JSON(http.StatusUnauthorized,gin.H{"code":http.StatusUnauthorized,"msg":"token已过期，请重新登录"})
 			ctx.Abort()
 			return
 		}
@@ -39,7 +39,7 @@ func AuthMiddleware() gin.HandlerFunc{
 
 		// 用户不存在
 		if user.ID == 0{
-			ctx.JSON(http.StatusUnauthorized,gin.H{"code":401,"msg":"没有权限哟"})
+			ctx.JSON(http.StatusUnauthorized,gin.H{"code":http.StatusUnauthorized,"msg":"没有权限哟"})
 			ctx.Abort()
 			return
 		}
